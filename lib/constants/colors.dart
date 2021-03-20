1bae48de37a11245dc008b5c821128e573a7ea38
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-03-20 21:16:39 +0900

    add: constant color

---
+++ lib/constants/colors.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const grey = Color(0xfff0f2f8);
+const darkGrey = Color(0xff989898);
+
+const white = Color.fromRGBO(255, 255, 255, 1);
+const backgroundWhite = Color(0xfff8f9fd);
+const black = Color(0xff272933);
+
+const darkBlack = Color.fromRGBO(1, 1, 1, 1);
+const slateGrey = Color.fromRGBO(87, 93, 112, 1.0);
+const paleGrey = Color.fromRGBO(240, 242, 248, 1.0);
+const warmGrey = Color.fromRGBO(152, 152, 152, 1.0);
+const deepGray = Color.fromRGBO(114, 90, 136, 1);
+const pink = Color(0xffff7c98);
+const pinky = Color(0xffff88b3);
+const deepPink = Color(0xffef2b76);
+
+const darkModeChatBackGroundColor = Color.fromRGBO(62, 83, 102, 1);
+const headerColor = Color.fromRGBO(20, 40, 67, 1.0);
+const chatScreenBackgroundColor = Color.fromRGBO(130, 160, 198, 1.0);
+
+// ダイアログの選択されていないボタンの色
+const secondButtonColor = Color.fromRGBO(248, 249, 253, 1);
+
+///=== 以下、色定義等 ==============================================
+///--- アプリ基本 ---------------------------------------
+const Color COLOR_WHITE = Colors.white; // 白系
+const Color COLOR_TEXT_STRONG = Colors.black45; // TEXT系 強調色
+const Color COLOR_TEXT_DEFAULT = Colors.black45; // TEXT系デフォルト
+const Color COLOR_TEXT_TONEDOWN = Colors.grey; // TEXT系デフォルト（トーンダウン気味）
+const Color COLOR_CONCEPT = pinky; // アプリコンセプト色
+const Color COLOR_CONCEPT_WEAK = Colors.lightBlue; // アプリコンセプト色2 (淡め）
+const Color COLOR_FLUSHBAR_BACK = Color(0xFFFFC9AF); // Flusbar背景色

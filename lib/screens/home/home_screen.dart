28a5a35c6f3d492bdb4ff583b9b6fb52fc7a0b0a
jiali li <lijiali0515@yahoo.co.jp>
2021-03-21 23:20:01 +0900

    update:homescreen foundteacher

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,22 +1,161 @@
 import "package:flutter/material.dart";
 import 'package:onetime/screens/mypage/buildClassIntroPage.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String text = '';
+
+  void chatbox(String input) {
+    text = input;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            ListView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                // header
+                UserAccountsDrawerHeader(
+                  //icon图标，当每上传照片时为登陆图标，上传照片后显示图片
+                  currentAccountPicture: GestureDetector(
+                    // onTap: () {
+                    //   Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //       builder: (context) => LoginPage(),
+                    //     ),
+                    //   );
+                    //   //Navigator.of(context).pushNamed('/chatpage');
+                    // },
+                    child: Row(
+                      children: [
+                        InkWell(
+                          // onTap: () {
+                          //   Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //       builder: (context) => LoginPage(),
+                          //     ),
+                          //   );
+                          // },
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 50,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  accountEmail: Text('@Onetime.com'),
+
+                  //背景
+                  decoration: BoxDecoration(
+                    color: Colors.blue[200],
+                  ),
+                ),
+
+                // make an account
+                ListTile(
+                    // title: GestureDetector(
+                    //   onTap: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => LoginPage(),
+                    //       ),
+                    //     );
+                    //     //Navigator.of(context).pushNamed('/chatpage');
+                    //   },
+                    //   child: Text('Make an account'),
+                    // ),
+                    // leading: GestureDetector(
+                    //   onTap: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => LoginPage(),
+                    //       ),
+                    //     );
+                    //     //Navigator.of(context).pushNamed('/chatpage');
+                    //   },
+                    //   child: Icon(
+                    //     Icons.account_circle,
+                    //     color: Colors.black,
+                    //     size: 30,
+                    //   ),
+                    // ),
+                    ),
+              ],
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.lightBlue,
       ),
-      body: Container(
-        child: ListView.builder(
-          shrinkWrap: true, // 高さ関連のエラーが出たら、使う
-          itemCount: 4,
-          itemBuilder: (BuildContext context, int index) {
-            return _buildClassTile(index + 1, 'introduction', context);
-          },
-        ),
+      body: Column(
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  '教師を探す',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          Form(
+            // key: formKey,
+            child: SizedBox(
+              height: 90,
+              width: 400,
+              child: ColoredBox(
+                color: Colors.white,
+                child: TextFormField(
+                  maxLines: 2,
+                  onChanged: (String text) {
+                    chatbox(text);
+                  },
+                  decoration: InputDecoration(
+                    hintText: '言語名、教師名などを入力してください',  
+                    contentPadding: const EdgeInsets.all(10),
+                    border: InputBorder.none,
+                    icon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: ListView.builder(
+              shrinkWrap: true, // 高さ関連のエラーが出たら、使う
+              itemCount: 4,
+              itemBuilder: (BuildContext context, int index) {
+                return _buildClassTile(index + 1, 'introduction', context);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -7,31 +7,31 @@ import 'package:onetime/models/theme/theme_mode_extension.dart';
 class SettingsScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final themeSelector = useProvider(themeSelectorProvider);
-    final currentThemeMode = useProvider(themeSelectorProvider.state);
+    // final themeSelector = useProvider(themeSelectorProvider);
+    // final currentThemeMode = useProvider(themeSelectorProvider.state);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('設定'),
       ),
-      body: SafeArea(
-        child: ListView.builder(
-          itemCount: ThemeMode.values.length,
-          itemBuilder: (_, index) {
-            final themeMode = ThemeMode.values[index];
-            return RadioListTile<ThemeMode>(
-              value: themeMode,
-              groupValue: currentThemeMode,
-              onChanged: (newTheme) {
-                themeSelector.change(newTheme);
-              },
-              title: Text(themeMode.title),
-              subtitle: Text(themeMode.subtitle),
-              controlAffinity: ListTileControlAffinity.platform,
-            );
-          },
-        ),
-      ),
+      // body: SafeArea(
+      //   child: ListView.builder(
+      //     itemCount: ThemeMode.values.length,
+      //     itemBuilder: (_, index) {
+      //       final themeMode = ThemeMode.values[index];
+      //       return RadioListTile<ThemeMode>(
+      //         value: themeMode,
+      //         groupValue: currentThemeMode,
+      //         onChanged: (newTheme) {
+      //           themeSelector.change(newTheme);
+      //         },
+      //         title: Text(themeMode.title),
+      //         subtitle: Text(themeMode.subtitle),
+      //         controlAffinity: ListTileControlAffinity.platform,
+      //       );
+      //     },
+      //   ),
+      // ),
     );
   }
 }

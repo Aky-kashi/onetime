c1d56d11f3dfe0d81595c0111b2b5b7d81266c25
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-03-21 21:32:18 +0900

    update: main - StatelessWidget changed to HookWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart' as riverPod;
 import 'package:onetime/screens/home/home_screen.dart';
 import 'package:onetime/screens/login/login_screen.dart';
@@ -28,7 +29,7 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     // final loginViewModel = Provider.of<LoginViewModel>(context, listen: false);

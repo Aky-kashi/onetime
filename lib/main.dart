13d73d46b1cba55b618dc21063f837287676ed14
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-03-21 13:43:11 +0900

    update: main - MyApp widget inherited HookWidget for fixing useProviders runtime error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart' as riverPod;
 import 'package:onetime/screens/home/home_screen.dart';
 import 'package:onetime/screens/login/login_screen.dart';
@@ -18,14 +19,16 @@ void main() async {
   await Firebase.initializeApp();
 
   runApp(
-    MultiProvider(
-      providers: globalProviders,
-      child: MyApp(),
+    riverPod.ProviderScope(
+      child: MultiProvider(
+        providers: globalProviders,
+        child: MyApp(),
+      ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final loginViewModel = Provider.of<LoginViewModel>(context, listen: false);
@@ -38,12 +41,12 @@ class MyApp extends StatelessWidget {
       home: FutureBuilder(
         future: loginViewModel.isSingIn(),
         builder: (context, AsyncSnapshot<bool> snapshot) {
-          return TabScreen();
-          // if (snapshot.hasData && snapshot.data) {
-          //   return HomeScreen();
-          // } else {
-          //   return LoginScreen();
-          // }
+          //return TabScreen();
+          if (snapshot.hasData && snapshot.data) {
+            return HomeScreen();
+          } else {
+            return LoginScreen();
+          }
         },
       ),
     );
